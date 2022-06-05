ee36ea4439dee2d977e33bf13fcc673f45c7d4f1
Ferencz David Tamas <ferencz.david@protonmail.com>
2022-06-05 13:21:45 +0200

    fix: use sass for system styles

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,7 @@
 import * as ReactDOMClient from "react-dom/client";
 import Layout from "./Layout/Layout";
 
-import "../System/Styles/styles.css";
+import "../System/Styles/styles.scss";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 const root = ReactDOMClient.createRoot(rootElement);

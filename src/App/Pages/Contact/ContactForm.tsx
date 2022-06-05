829c1e8831b429eb11e776568b0c949404bf6164
Ferencz David Tamas <ferencz.david@protonmail.com>
2022-06-05 23:14:08 +0200

    feat: remove unnecessary code from Contact Form

--- src/App/Pages/Contact/ContactForm.tsx
+++ src/App/Pages/Contact/ContactForm.tsx
@@ -1,6 +1,6 @@
 import { Expose } from "class-transformer";
-import { IsEmail, MinLength, validate, ValidationError } from "class-validator";
-import { computed, makeObservable, observable } from "mobx";
+import { IsEmail, MinLength } from "class-validator";
+import {makeObservable, observable } from "mobx";
 import BaseSerializableModel from "../../../System/Models/BaseSerializableModel";
 
 class ContactForm extends BaseSerializableModel {
@@ -18,49 +18,9 @@ class ContactForm extends BaseSerializableModel {
   @Expose()
   message: string = "";
 
-  @observable
-  errors: ValidationError[] = [];
-
-  @computed
-  get isValid(): boolean {
-    return this.errors.length === 0;
-  }
-
   constructor() {
     super();
-    this.validate();
     makeObservable(this);
-  }
-
-  async validate() {
-    this.errors = await validate(this);
-  }
-
-  getPropertyErrors(property: string): string[] {
-    const propertyError: ValidationError | undefined = this.errors.find(
-      (error, index) => error.property === property
-    );
-    if (!propertyError) {
-      return [];
-    }
-    return Object.keys(propertyError.constraints || {}).reduce(
-      (errors: string[], key) => {
-        if (
-          propertyError &&
-          propertyError.constraints &&
-          propertyError.constraints[key]
-        ) {
-          errors.push(propertyError.constraints[key]);
-        }
-
-        return errors;
-      },
-      []
-    );
-  }
-
-  hasPropertyError(property: string): boolean {
-    return !!this.errors.find((error) => error.property === property);
   }
 }
 

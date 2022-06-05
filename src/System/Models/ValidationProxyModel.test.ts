189e83db148d86f86aca41abb1bd5fd1be7e5fc2
Ferencz David Tamas <ferencz.david@protonmail.com>
2022-06-05 19:17:55 +0200

    feat: validation proxy model

---
+++ src/System/Models/ValidationProxyModel.test.ts
@@ -0,0 +1,42 @@
+import { IsEmail, MinLength } from "class-validator";
+import { makeObservable, observable } from "mobx";
+import * as ProxyModel from './ProxyModel';
+import * as ValidationProxy from './ValidationProxy';
+import { createValidationProxyModel } from "./ValidationProxyModel";
+
+class ValidModel {
+    @observable
+    @MinLength(2, { message: 'Name must be at least 2 characters long!'})
+    name: string = "";
+
+    @observable
+    @IsEmail()
+    email: string = "";
+
+    constructor() {
+        makeObservable(this);
+    }
+}
+
+describe("Test ValidationProxyModel", () => {
+  test("Can Create ValidationProxyModel", () => {
+    const validModel = new ValidModel();
+    expect(() => createValidationProxyModel(validModel)).not.toThrow();
+  });
+
+  test("Create calls createValidationProxy and createProxyModel", () => {
+    const validModel = new ValidModel();
+    const createProxyModelSpy = jest.spyOn(ProxyModel, 'createProxyModel');
+    const createValidationProxySpy = jest.spyOn(ValidationProxy, 'createValidationProxy');
+    createValidationProxyModel(validModel);
+
+    expect(createProxyModelSpy).toBeCalledTimes(1);
+    expect(createValidationProxySpy).toBeCalledTimes(1);
+  });
+
+  test("Create returns object", () => {
+    const validModel = new ValidModel();
+    expect(createValidationProxyModel(validModel)).not.toBeNull();
+  });
+
+});
--- src/System/Models/ValidationProxyModel.ts
+++ src/System/Models/ValidationProxyModel.ts
@@ -1,8 +1,8 @@
 import { createProxyModel, ProxyModel } from './ProxyModel';
-import { ValidationProxyType } from './ValidationProxy';
+import { createValidationProxy, ValidationProxyType } from './ValidationProxy';
 
 export type ValidationProxyModel<T> = ValidationProxyType<T> & ProxyModel<T>;
 
 export const createValidationProxyModel = <T extends object>(model: T): ValidationProxyModel<T> => {
-    return createValidationProxyModel(createProxyModel(model));
+    return createValidationProxy(createProxyModel(model));
 };

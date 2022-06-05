c0c43c9b0c42988d5af7f516b518ea524cf3dfc9
Ferencz David Tamas <ferencz.david@protonmail.com>
2022-06-05 23:13:01 +0200

    feat: add loader test

---
+++ src/System/Components/Loader/Loader.test.tsx
@@ -0,0 +1,9 @@
+import { act } from "react-test-renderer";
+import { render } from "@testing-library/react";
+import Loader from "./Loader";
+
+test("Can render Loader component", () => {
+  act(() => {
+    render(<Loader />);
+  });
+});

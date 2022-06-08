ef13a2c32125c1eb86cce7a0a951152fba7842c3
Ferencz David Tamas <ferencz.david@protonmail.com>
2022-06-08 21:57:55 +0200

    feat: BindModel tests

--- src/System/Components/BindModel/BindModel.test.tsx
+++ src/System/Components/BindModel/BindModel.test.tsx
@@ -23,89 +23,88 @@ class ValidModel {
   }
 }
 
-test("Can render BindModel component", async () => {
-  const model = new ValidModel();
+describe("BindModel test suite", () => {
 
-  const { getByTestId } = render(<BindModel model={model} property="name" extraProps={{variant: 'solid'}}>
-    <Input data-testid="testInput" label="Test" />
-  </BindModel>);
+  test("Can render BindModel component", async () => {
+    const model = new ValidModel();
 
-  const inputElement = getByTestId('testInput').querySelector('input') as HTMLInputElement;
-  userEvent.type(inputElement, 'test', { delay: 2 });
-  await new Promise((r) => setTimeout(r, 300));
-  expect(model.name).toEqual('test');
-  expect(inputElement.value).toEqual('test');
-});
+    const { getByTestId } = render(<BindModel model={model} property="name" extraProps={{variant: 'solid'}}>
+      <Input data-testid="testInput" label="Test" />
+    </BindModel>);
 
-
-test("Uses data transofmer", async () => {
-  const model = new ValidModel();
-
-  const transformToInSpy = jest.spyOn(TrimDataTransformer, 'transformToIn');
-  const transformToOutSpy = jest.spyOn(TrimDataTransformer, 'transformToOut');
-
-  transformToInSpy.mockClear();
-  transformToOutSpy.mockClear();
-
-  const { getByTestId } = render(<BindModel model={model} property="name" dataTransformers={[TrimDataTransformer]}>
-    <Input data-testid="testInput" label="Test" />
-  </BindModel>);
-
-  const inputElement = getByTestId('testInput').querySelector('input') as HTMLInputElement;
-  userEvent.type(inputElement, '  test', { delay: 2 });
-  await new Promise((r) => setTimeout(r, 300));
-  expect(model.name).toEqual('test');
-  expect(inputElement.value).toEqual('test');
- 
-  // FIXME: why do the toIn runs onyl 5 times?
-  expect(transformToInSpy).toBeCalledTimes(5);
-  expect(transformToOutSpy).toBeCalledTimes(6);
-});
+    const inputElement = getByTestId('testInput').querySelector('input') as HTMLInputElement;
+    userEvent.type(inputElement, 'test', { delay: 2 });
+    await new Promise((r) => setTimeout(r, 300));
+    expect(model.name).toEqual('test');
+    expect(inputElement.value).toEqual('test');
+  });
 
 
-test("Works with checkbox input", async () => {
-  const model = new ValidModel();
+  test("Uses data transofmer", async () => {
+    const model = new ValidModel();
 
-  const { getByTestId } = render(<BindModel model={model} property="adult">
-    <Checkbox data-testid="testInput" label="Is Adult?" />
-  </BindModel>);
+    const transformToInSpy = jest.spyOn(TrimDataTransformer, 'transformToIn');
+    const transformToOutSpy = jest.spyOn(TrimDataTransformer, 'transformToOut');
 
-  const checkboxElement = getByTestId('testInput').querySelector('input') as HTMLInputElement;
+    transformToInSpy.mockClear();
+    transformToOutSpy.mockClear();
+
+    const { getByTestId } = render(<BindModel model={model} property="name" dataTransformers={[TrimDataTransformer]}>
+      <Input data-testid="testInput" label="Test" />
+    </BindModel>);
+
+    const inputElement = getByTestId('testInput').querySelector('input') as HTMLInputElement;
+    userEvent.type(inputElement, '  test', { delay: 2 });
+    await new Promise((r) => setTimeout(r, 300));
+    expect(model.name).toEqual('test');
+    expect(inputElement.value).toEqual('test');
   
-  act(() => {
-    fireEvent.click(checkboxElement)
+    // FIXME: why do the toIn runs onyl 5 times?
+    expect(transformToInSpy).toBeCalledTimes(5);
+    expect(transformToOutSpy).toBeCalledTimes(6);
   });
-  expect(checkboxElement).toBeTruthy();
 
-  act(() => {
-    fireEvent.click(checkboxElement);
+
+  test("Works with checkbox input", async () => {
+    const model = new ValidModel();
+
+    const { getByTestId } = render(<BindModel model={model} property="adult">
+      <Checkbox data-testid="testInput" label="Is Adult?" />
+    </BindModel>);
+
+    const checkboxElement = getByTestId('testInput').querySelector('input') as HTMLInputElement;
+    
+    act(() => {
+      fireEvent.click(checkboxElement)
+    });
+    expect(model.adult).toBeTruthy();
+
+    act(() => {
+      fireEvent.click(checkboxElement);
+    });
+    expect(model.adult).toBeFalsy();
   });
-  expect(model.adult).toBeFalsy();
-});
 
 
-test("Works with object", async () => {
-  const model = new ValidModel();
+  test("Works with object", async () => {
+    const model = new ValidModel();
 
-  const ControllerComponent: FC<ControlledModelProps<boolean> & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = ({onChange, value, ...props}) => (
-    <div onClick={() => onChange?.(!!value)} {...props}>
-      {value ? 'true' : 'false'}
-    </div>
-  );
+    const ControllerComponent: FC<ControlledModelProps<boolean> & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>> = ({onChange, value, ...props}) => (
+      <div onClick={() => onChange?.(!!value)} {...props}>
+        {value ? 'true' : 'false'}
+      </div>
+    );
 
-  const { getByTestId } = render(<BindModel model={model} property="adult">
-    <ControllerComponent data-testid="testInput" />
-  </BindModel>);
+    const { getByTestId } = render(<BindModel model={model} property="adult">
+      <ControllerComponent data-testid="testInput" />
+    </BindModel>);
 
-  const customElement = getByTestId('testInput') as HTMLDivElement;
-  
-  act(() => {
-    fireEvent.click(customElement)
+    const customElement = getByTestId('testInput') as HTMLDivElement;
+    
+    fireEvent.click(customElement);
+    expect(customElement).toBeTruthy();
+
+    fireEvent.click(customElement!);
+    expect(model.adult).toBeFalsy();
   });
-  expect(customElement).toBeTruthy();
-
-  act(() => {
-    userEvent.click(customElement!);
-  });
-  expect(model.adult).toBeFalsy();
 });

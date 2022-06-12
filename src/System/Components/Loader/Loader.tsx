d38526ccec22470ba19b3099c37a8e386e84e3f7
Ferencz David Tamas <ferencz.david@protonmail.com>
2022-06-12 02:05:58 +0200

    docs: add loader lottifile source

--- src/System/Components/Loader/Loader.tsx
+++ src/System/Components/Loader/Loader.tsx
@@ -1,5 +1,7 @@
 import { memo } from 'react';
 import animation from './animation.json';
+
+// @source https://lottiefiles.com/99584-loading-23
 import { Player } from '@lottiefiles/react-lottie-player';
 
 const Loader = () => (

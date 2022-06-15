21373fcf0ca9a127a59d1bf86324dcf4cae411f0
Ferencz David Tamas <ferencz.david@protonmail.com>
2022-06-15 22:55:48 +0200

    feat: Products grid

--- src/App/Pages/Product/ProductGrid/ProductGrid.tsx
+++ src/App/Pages/Product/ProductGrid/ProductGrid.tsx
@@ -1,35 +1,92 @@
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-import { Card, CardContent } from '@mui/material';
+import {
+	Button,
+	Card,
+	CardContent,
+	IconButton,
+	Typography,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
 import FadeInOut from '@System/Components/Animations/FadeInOut/FadeInOut';
+import {
+	Add,
+	Delete,
+	Edit,
+	QuestionMark,
+	Visibility,
+} from '@mui/icons-material';
 
 const rows: GridRowsProp = [
-	{ id: 1, col1: 'Hello', col2: 'World' },
-	{ id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-	{ id: 3, col1: 'MUI', col2: 'is Amazing' },
+	{ id: 1, name: 'Test device' },
+	{ id: 2, name: 'DataGridPro' },
+	{ id: 3, name: 'MUI' },
+	{ id: 4, name: 'MUI2' },
+	{ id: 5, name: 'MUI3' },
+	{ id: 6, name: 'MUI4' },
+	{ id: 7, name: 'MUI5' },
 ];
 
 const columns: GridColDef[] = [
+	{ field: 'name', headerName: 'Name', flex: 5 },
 	{
-		field: 'col1',
+		field: 'id',
+		type: 'actions',
 		headerName: 'Actions',
-		width: 150,
 		renderCell: (cellValues) => (
 			<>
-				<Link to={`/products/${cellValues.id}/view`}>View</Link>
-				<Link to={`/products/${cellValues.id}/edit`}>Edit</Link>
+				<Link to={`/products/${cellValues.id}/view`}>
+					<IconButton
+						color="primary"
+						aria-label="Product Details"
+						component="span"
+					>
+						<Visibility />
+					</IconButton>
+				</Link>
+				<Link to={`/products/${cellValues.id}/edit`}>
+					<IconButton
+						color="primary"
+						aria-label="Edit Product"
+						component="span"
+					>
+						<Edit />
+					</IconButton>
+				</Link>
+				<IconButton color="error" aria-label="Delete Product" component="span">
+					<Delete />
+				</IconButton>
 			</>
 		),
+		flex: 1,
 	},
-	{ field: 'col2', headerName: 'Column 2', width: 150 },
 ];
 
 const ProductGrid = () => (
 	<FadeInOut>
+		<div className="grid mb-4">
+			<div className="col-6">
+				<Typography variant="h5">Products</Typography>
+			</div>
+			<div className="col-6 text-right">
+				<Link to="/products/info">
+					<IconButton
+						aria-label="About Products"
+						component="span"
+						className="mr-4"
+					>
+						<QuestionMark />
+					</IconButton>
+				</Link>
+				<Link to="/products/create">
+					<Button variant="contained" startIcon={<Add />}>
+						Add Product
+					</Button>
+				</Link>
+			</div>
+		</div>
 		<Card>
-			<CardContent>
-				<Link to={'/products/info'}>Info</Link>
-				<div style={{ height: 300, width: '100%' }}>
+			<CardContent className="m-0 p-0">
+				<div style={{ height: 500, width: '100%' }}>
 					<DataGrid rows={rows} columns={columns} />
 				</div>
 			</CardContent>

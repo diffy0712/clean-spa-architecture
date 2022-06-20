55702663205e5dd7022f5dd485057a92b6f11cd9
Ferencz David Tamas <ferencz.david@protonmail.com>
2022-06-21 00:12:36 +0200

    feat(Input): unit tests with touchError

---
+++ src/System/Components/FormControl/Input/Input.test.tsx
@@ -0,0 +1,127 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Input from '@System/Components/FormControl/Input/Input';
+import { sleep } from '@System/Utils/async';
+import { act } from 'react-test-renderer';
+
+describe('Input test suite', () => {
+	test('Can render Input component', async () => {
+		const inputOnChangeMock = jest.fn(
+			(eventOrValue: React.ChangeEvent<HTMLInputElement> | string) => {
+				expect(
+					typeof eventOrValue !== 'string' ? eventOrValue?.target.value : ''
+				).toEqual('testt');
+			}
+		);
+		const { getByTestId } = render(
+			<Input
+				data-testid="testInput"
+				value="test"
+				onChange={inputOnChangeMock}
+			/>
+		);
+
+		const inputElement = getByTestId('testInput').querySelector(
+			'input'
+		) as HTMLInputElement;
+		expect(inputElement.value).toEqual('test');
+
+		await userEvent.type(inputElement, 't');
+		await sleep(200);
+		expect(inputOnChangeMock).toBeCalledTimes(1);
+	});
+
+	test('Render Input component with label', async () => {
+		const inputOnChangeMock = jest.fn(() => true);
+		const { queryByText, queryByTestId } = render(
+			<Input
+				value="test"
+				data-testid="testInput"
+				label="Input Mock Test Label"
+				onChange={inputOnChangeMock}
+			/>
+		);
+
+		await sleep(50);
+		expect(queryByTestId('testInput-label')).not.toBeNull();
+		expect(queryByText('Input Mock Test Label')).not.toBeNull();
+	});
+
+	test('Render Input component without label', async () => {
+		const inputOnChangeMock = jest.fn(() => true);
+		const { queryByTestId } = render(
+			<Input
+				value="test"
+				data-testid="testInput"
+				onChange={inputOnChangeMock}
+			/>
+		);
+
+		await sleep(50);
+		expect(queryByTestId('testInput-label')).toBeNull();
+	});
+
+	test('Render Input component with errors and touchError', async () => {
+		const inputOnChangeMock = jest.fn(() => true);
+		const { queryByText, queryByTestId, getByTestId } = render(
+			<Input
+				value="test"
+				data-testid="testInput"
+				onChange={inputOnChangeMock}
+				errors={['Error1', 'Error2']}
+			/>
+		);
+
+		await sleep(50);
+		expect(queryByTestId('testInput-errors')).toBeNull();
+		expect(queryByText('Error1')).toBeNull();
+		expect(queryByText('Error2')).toBeNull();
+
+		const inputElement = getByTestId('testInput').querySelector(
+			'input'
+		) as HTMLInputElement;
+
+		await act(() => {
+			inputElement.focus();
+			inputElement.blur();
+		});
+
+		expect(queryByTestId('testInput-errors')).not.toBeNull();
+		expect(queryByText('Error1')).not.toBeNull();
+		expect(queryByText('Error2')).not.toBeNull();
+	});
+
+	test('Render Input component with errors and touchError disabled', async () => {
+		const inputOnChangeMock = jest.fn(() => true);
+		const { queryByText, queryByTestId, getByTestId, debug } = render(
+			<Input
+				value="test"
+				data-testid="testInput"
+				onChange={inputOnChangeMock}
+				errors={['Error1', 'Error2']}
+				touchErrors={false}
+			/>
+		);
+
+		debug();
+		await sleep(50);
+		expect(queryByTestId('testInput-errors')).not.toBeNull();
+		expect(queryByText('Error1')).not.toBeNull();
+		expect(queryByText('Error2')).not.toBeNull();
+	});
+
+	test('Render Input component without errors', async () => {
+		const inputOnChangeMock = jest.fn(() => true);
+		const { queryByTestId } = render(
+			<Input
+				value="test"
+				data-testid="testInput"
+				onChange={inputOnChangeMock}
+				errors={[]}
+				touchErrors={false}
+			/>
+		);
+
+		expect(queryByTestId('testInput-errors')).toBeNull();
+	});
+});
--- src/System/Components/FormControl/Input/Input.tsx
+++ src/System/Components/FormControl/Input/Input.tsx
@@ -5,27 +5,32 @@ import {
 	InputProps as MUIInputProps,
 	Input as MUIInput,
 } from '@mui/material';
-import { ChangeEvent, Fragment, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import ControlledValidationModelProps from '@System/Components/Props/ControlledValidationModelProps';
 
 type InputProps<T> = {
 	label?: string;
+	touchErrors?: boolean;
 } & ControlledValidationModelProps<T, React.ChangeEvent<HTMLInputElement>> &
 	Omit<MUIInputProps, 'onChange' | 'value'>;
 
 const Input = <T extends string | number | readonly string[]>({
 	label,
+	touchErrors,
 	errors: propErrors,
 	onChange,
 	onBlur,
 	...props
 }: InputProps<T>) => {
-	const [isTouched, setIsTouched] = useState<boolean>(false);
+	const [isTouched, setIsTouched] = useState<boolean>(!touchErrors);
 	const errors = isTouched ? propErrors : [];
+
+	const dataTestId = props['data-testid'];
 
 	const onTouchedBlur = (
 		event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
 	) => {
+		console.log('bluring he');
 		setIsTouched(true);
 		onBlur?.(event);
 	};
@@ -36,7 +41,11 @@ const Input = <T extends string | number | readonly string[]>({
 			variant="standard"
 			className="w-full"
 		>
-			{label && <InputLabel htmlFor={props.id}>{label}</InputLabel>}
+			{label && (
+				<InputLabel data-testid={`${dataTestId}-label`} htmlFor={props.id}>
+					{label}
+				</InputLabel>
+			)}
 			<MUIInput
 				type="text"
 				onChange={(event: ChangeEvent<HTMLInputElement>) => {
@@ -44,14 +53,17 @@ const Input = <T extends string | number | readonly string[]>({
 				}}
 				minRows={2}
 				maxRows={10}
-				aria-describedby="component-error-text"
+				aria-describedby="form-control-input-errors"
 				onBlur={onTouchedBlur}
 				{...props}
 			/>
 			{errors && errors.length > 0 && (
-				<FormHelperText id="component-error-text">
+				<FormHelperText
+					id="form-control-input-errors"
+					data-testid={`${dataTestId}-errors`}
+				>
 					{errors?.map((error) => (
-						<Fragment key={error}>{error}</Fragment>
+						<span key={error}>{error}</span>
 					))}
 				</FormHelperText>
 			)}
@@ -61,6 +73,7 @@ const Input = <T extends string | number | readonly string[]>({
 
 Input.defaultProps = {
 	errors: [],
+	touchErrors: true,
 };
 
 export default Input;

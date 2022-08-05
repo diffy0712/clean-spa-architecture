f83edacdb9be677d2e68d6df7ce0025311c73abd
Ferencz David Tamas <ferencz.david@protonmail.com>
2022-08-05 21:14:49 +0200

    refactor(Routes): extract static methods from components

--- src/App/Layout/Routes.tsx
+++ src/App/Layout/Routes.tsx
@@ -14,25 +14,26 @@ const ProductModule = React.lazy(
 	() => import('@App/Pages/Product/ProductModule')
 );
 
+const modules: [string, ReactElement][] = [
+	['/products', <ProductModule key="products" />],
+];
+
+// With the location.pathname defined as key, when navigating between
+// sub routes, the change will rerender (reroute) the whole tree.
+// By narrowing the location key, to be only the modules, main route
+// the navigation between sub routes of the same group, will only rerender (reroute)
+// inside the sub route branch.
+//
+// Using animations this problem is visible. Just return the location.pathname as key :)
+const getLocationKey = () => {
+	const module = modules.find(
+		(_module) => location.pathname.indexOf(_module[0]) >= 0
+	);
+	return module ? module[0] : location.pathname;
+};
+
 const Routes = () => {
 	const location = useLocation();
-	const modules: [string, ReactElement][] = [
-		['/products', <ProductModule key="products" />],
-	];
-
-	// With the location.pathname defined as key, when navigating between
-	// sub routes, the change will rerender (reroute) the whole tree.
-	// By narrowing the location key, to be only the modules, main route
-	// the navigation between sub routes of the same group, will only rerender (reroute)
-	// inside the sub route branch.
-	//
-	// Using animations this problem is visible. Just return the location.pathname as key :)
-	const getLocationKey = () => {
-		const module = modules.find(
-			(_module) => location.pathname.indexOf(_module[0]) >= 0
-		);
-		return module ? module[0] : location.pathname;
-	};
 
 	return (
 		<Suspense fallback={<Loader />}>
